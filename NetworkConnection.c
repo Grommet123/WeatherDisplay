efde651bc70345ace0c199309fb0952d815eb84a
Gary Grotsky <gkgrotsky@aol.com>
2017-01-26 14:50:10 -0500

    Initial release

---
+++ NetworkConnection.c
@@ -0,0 +1,4 @@
+const char* ssid     = "Your ssid"; // SSID of local network
+const char* password = "Your password"; // Password on network
+const char* APIKey   = "Your API key"; // API key of openweathermap.org
+const char* cityID   = "Your city ID"; // City ID of openweathermap.org
