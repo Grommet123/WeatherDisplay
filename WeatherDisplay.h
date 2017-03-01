4bd17bd4cf233e9d716467e5331f33524ef6ff27
Gary Grotsky <gkgrotsky@aol.com>
2017-03-01 10:27:06 -0500

    v8.3 Minor clean-up

--- WeatherDisplay.h
+++ WeatherDisplay.h
@@ -8,7 +8,7 @@
 #define WeatherDisplay_h
 #include <Arduino.h>
 
-#define VERSION  "8.2"         // Version number
+#define VERSION  "8.3"         // Version number
 #define GETDATACOUNT 60 * 30   // Time to get more data (in seconds)
 #define PORT 80                // Port number of remote server
 #define ERROROFFSET            // Add time/date error offset
